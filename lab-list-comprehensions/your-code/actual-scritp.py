33296edc969ba050e4705270fb7b00e1a4de220f
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-12 14:40:40 -0500

    done until exersice 7

--- lab-list-comprehensions/your-code/actual-scritp.py
+++ lab-list-comprehensions/your-code/actual-scritp.py
@@ -7,12 +7,12 @@ print(msg)
 msg = "Hola Mundo"
 print(msg)
 # %%
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 df = pd.read_csv("../data/sample_file_0.csv")
 df.head(10)
-plt.plot(df)
 
+# %%
+import os
+csv_files = [csv_file for csv_file in listdir("../data")
+#csv_files = [ filename for filename in filenames if filename.endswith(".csv") ]
 # %%
